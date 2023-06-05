c989cb42ec56fc02718ca8906444b15d0b9f96d3
AlexPolyakovsky <133758500+AlexPolyakovsky@users.noreply.github.com>
2023-06-05 16:04:13 +0300

    [ACDEV-846] Authentication methods upgrade  (#1)
    * Add signature auth service
    
    * Update readme
    
    * Minor fixes
    
    * Fix misprint

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ docker build -t appcharge/webhook-ruby .
 
 ### How to run the container locally
 docker run -p8080:8080 --rm -it -e KEY='[KEY GOES HERE]' -e IV='[IV GOES HERE]' -e APP_SECRET='[FACEBOOK APP SECRET]' appcharge/webhook-ruby
+
+### Where to get the key
+Go to the admin panel, open the integration tab, if you are using signature authentication - copy the key from the primary key field, if you are using encryption, take the key from the primary key field and the IV from the secondary key field.
---
+++ controllers/signature.rb
@@ -0,0 +1,47 @@
+require 'openssl'
+
+key = ENV['KEY']
+
+
+class SignatureService
+
+  def self.init(key)
+    @@key = key
+  end
+
+  def self.get_signature(payload)
+    digest = OpenSSL::Digest::SHA256.new
+    hmac = OpenSSL::HMAC.digest(digest, @@key, payload)
+    signature = hmac.unpack1('H*')
+    return signature
+  end
+
+  def self.parse_signature(signature)
+    regex = /t=(.*),v1=(.*)/
+    match = regex.match(signature)
+    unless match && match.size >= 3
+      raise ArgumentError, "Invalid signature format"
+    end
+    timestamp = match[1]
+    signature = match[2]
+    return timestamp, signature
+
+  end
+
+  def self.serialize_payload(payload, timestamp)
+    payload_hashmap = JSON.parse(payload)
+    formatted_payload = JSON.generate(payload_hashmap, space: "").to_s
+    serialized_payload = "%s.%s" % [timestamp, formatted_payload]
+    return serialized_payload
+  end
+
+  def self.check_signature(payload, signature)
+    timestamp, signature = self.parse_signature(signature)
+    serialized_payload = self.serialize_payload(payload, timestamp)
+    expected_signature = self.get_signature(serialized_payload)
+    return expected_signature == signature
+  end
+
+end
+
+SignatureService.init(key)
